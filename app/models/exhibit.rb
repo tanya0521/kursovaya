6d7ff8423edf0a2bd28a549cff9fe83a64886992
Tanya <leonova.tatiana2011@yandex.ru>
2017-05-01 15:26:59 +0300

    Init

---
+++ app/models/exhibit.rb
@@ -0,0 +1,10 @@
+class Exhibit < ActiveRecord::Base
+  belongs_to :collection
+
+  validates :e_name, presence: true
+  validates :description_e, presence: true
+  validates :i_value, presence: true
+  validates :age, presence: true
+  validates :size, presence: true
+  validates :condition, presence: true
+end
